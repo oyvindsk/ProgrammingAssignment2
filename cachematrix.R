8dff0e845726ca8ba7f46832de910583359e450f
Øyvind Skaar <me@odots.org>
2015-03-22 22:11:11 +0100

    Typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,33 @@
 
-## This file is the solution to programming assignment 2 and contains 2 functions.
+## This file is the solution to programming assignment 2 and contains two functions.
 
-## Make a list of functions that work on a captured varible to store a matrix and a cached version of the inverse of the same matrix. 
-## No computation in this funxtion, only getter & setter functions that work on the captured variable.
+
+## Make a list of functions that work on a captured varible.
+## Store a matrix and a cached version of the inverse of the same matrix. 
+## No computation in this function, only getter & setter functions that work on the captured variable.
 
 makeCacheMatrix <- function(x = matrix()) {
 
-    ## Store the cached inverse matrix "inside" this closure
+    # Store the cached inverse matrix "inside" this closure (stored in the environment)
     cache <- NULL
     
-    ## Getter / setter functions to return
+    # Getter / setter functions to return
     get         <- function()  x
     getInverse  <- function()  cache
     setInverse  <- function(m) cache <<- m 
     
-    ## return a list of functions, like the example code
+    # Return a list of functions, like the example code
     list(get = get, getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Solve the inverse of a matrix, using the closue we created with makeCacheMatrix
-## If the cache is set return that. If not, solve() for matrix and return the result after setting the cache
+## Solve the inverse of a matrix, using the closure we created with makeCacheMatrix
+## If the cache is set return that. 
+## If not, solve() for matrix and return the result after setting the cache
 
 cacheSolve <- function(x, ...) {
     
-    inv <- x$getInverse() # # the varibale we will return after setting it to the correct matrix
+    inv <- x$getInverse() # the varibale we will return after setting it to the correct matrix
     
     if(is.null(inv)){
         # Nothing cached, solve and fill the cache
